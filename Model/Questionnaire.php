87fbc31310aa9db9c580af5a0a19c630b3ae01d0
tsmsogn <tsmsogn@gmail.com>
2014-10-22 14:01:42 +0900

    remove description not empty validation

--- Model/Questionnaire.php
+++ Model/Questionnaire.php
@@ -35,16 +35,6 @@ class Questionnaire extends TinyQuestionnaireAppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'description' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'status' => array(
 			'boolean' => array(
 				'rule' => array('boolean'),
