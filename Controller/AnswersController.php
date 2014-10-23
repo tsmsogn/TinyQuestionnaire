345d516bdc7fb1c1ef544cd28e0d6aad776409a7
tsmsogn <tsmsogn@gmail.com>
2014-10-23 10:38:35 +0900

    bake AnswersController

---
+++ Controller/AnswersController.php
@@ -0,0 +1,111 @@
+<?php
+App::uses('TinyQuestionnaireAppController', 'TinyQuestionnaire.Controller');
+/**
+ * Answers Controller
+ *
+ * @property Answer $Answer
+ * @property PaginatorComponent $Paginator
+ * @property SessionComponent $Session
+ */
+class AnswersController extends TinyQuestionnaireAppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator', 'Session');
+
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->Answer->recursive = 0;
+		$this->set('answers', $this->Paginator->paginate());
+	}
+
+/**
+ * admin_view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_view($id = null) {
+		if (!$this->Answer->exists($id)) {
+			throw new NotFoundException(__('Invalid answer'));
+		}
+		$options = array('conditions' => array('Answer.' . $this->Answer->primaryKey => $id));
+		$this->set('answer', $this->Answer->find('first', $options));
+	}
+
+/**
+ * admin_add method
+ *
+ * @return void
+ */
+	public function admin_add() {
+		if ($this->request->is('post')) {
+			$this->Answer->create();
+			if ($this->Answer->save($this->request->data)) {
+				$this->Session->setFlash(__('The answer has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The answer could not be saved. Please, try again.'));
+			}
+		}
+		$questions = $this->Answer->Question->find('list');
+		$users = $this->Answer->User->find('list');
+		$this->set(compact('questions', 'users'));
+	}
+
+/**
+ * admin_edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_edit($id = null) {
+		if (!$this->Answer->exists($id)) {
+			throw new NotFoundException(__('Invalid answer'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Answer->save($this->request->data)) {
+				$this->Session->setFlash(__('The answer has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The answer could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Answer.' . $this->Answer->primaryKey => $id));
+			$this->request->data = $this->Answer->find('first', $options);
+		}
+		$questions = $this->Answer->Question->find('list');
+		$users = $this->Answer->User->find('list');
+		$this->set(compact('questions', 'users'));
+	}
+
+/**
+ * admin_delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_delete($id = null) {
+		$this->Answer->id = $id;
+		if (!$this->Answer->exists()) {
+			throw new NotFoundException(__('Invalid answer'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Answer->delete()) {
+			$this->Session->setFlash(__('The answer has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The answer could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+}
